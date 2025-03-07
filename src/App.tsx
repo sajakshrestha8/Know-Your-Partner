4ad29c273a1af6c552d961e3879aa1f6bc0df854
sajakshrestha8 <shresthasajak123@gmail.com>
2025-03-07 23:00:07 +0545

    idea setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 const App = () => {
+  const data: object[] = [
+    {
+      question: "test question",
+      answer: "test answer",
+    },
+  ];
+  console.log(data);
+
   return (
     <>
-      <h1>Working</h1>
+      <h1>Know Your partner</h1>
+      {data?.map(({ question, answer }, index) => {
+        return (
+          <>
+            <label key={index}>{answer}</label>
+            <label key={index}>{question}</label>
+          </>
+        );
+      })}
     </>
   );
 };
