ef287f1e54da6f8844fa2a9b840df10feeea1382
sajakshrestha8 <shresthasajak123@gmail.com>
2025-03-09 13:20:29 +0545

    updated functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,67 @@
+import { ChangeEvent, useState } from "react";
+import "./App.css";
+
 const App = () => {
-  const data: object[] = [
+  interface Qna {
+    id: number;
+    question: string;
+    answer: string;
+  }
+  const [usedAnswer, setUsedAnswer] = useState<string>("");
+  const [checkAnswer, setCheckAnswer] = useState<boolean>(false);
+
+  const data: Array<Qna> = [
     {
-      question: "test question",
-      answer: "test answer",
+      id: 1,
+      question: "What is my name",
+      answer: "sajak shrestha",
+    },
+    {
+      id: 2,
+      question: "When is my birthday",
+      answer: "Ashoj 15",
     },
   ];
-  console.log(data);
+
+  const handleAnswer = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsedAnswer(e.target.value);
+  };
+
+  const handleSubmit = (a: string) => {
+    const inputAnswer = usedAnswer.toLowerCase();
+    console.log(inputAnswer);
+    console.log(a);
+    inputAnswer === a ? setCheckAnswer(true) : setCheckAnswer(false);
+    console.log(checkAnswer);
+  };
 
   return (
     <>
-      <h1>Know Your partner</h1>
-      {data?.map(({ question, answer }, index) => {
-        return (
-          <>
-            <label key={index}>{answer}</label>
-            <label key={index}>{question}</label>
-          </>
-        );
-      })}
+      <div className="wrapper">
+        <div className="Headings">
+          <label>Know Your partner</label>
+        </div>
+        {data?.map(({ id, question }, index) => {
+          return (
+            <>
+              <div key={index} className="qnaWrapper">
+                <div className="question">
+                  <label>
+                    {id}. {question}
+                  </label>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Enter your answer"
+                  onChange={handleAnswer}
+                />
+                <label>{checkAnswer ? checkAnswer + "" : ""}</label>
+                <button onClick={() => handleSubmit(answer)}>Submit</button>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </>
   );
 };
