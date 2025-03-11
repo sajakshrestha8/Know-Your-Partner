5a98ccaaf169acbb03a03f7992722c3fc4ec9861
sajakshrestha8 <shresthasajak123@gmail.com>
2025-03-11 17:58:33 +0545

    working on further other functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { ChangeEvent, useState } from "react";
 import "./App.css";
+import { TextField } from "@mui/material";
 
 const App = () => {
   interface Qna {
@@ -40,6 +41,16 @@ const App = () => {
     },
   ];
 
+  const x: number = 20;
+  const finalMessage: string =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+  const percetage: number = (x / 100) * finalMessage.length;
+
+  const slicedMessage: string = finalMessage.slice(0, percetage);
+
+  console.log((x / 100) * finalMessage.length);
+
   const handleAnswer = (e: ChangeEvent<HTMLInputElement>) => {
     setUsedAnswer(e.target.value);
   };
@@ -74,12 +85,13 @@ const App = () => {
                 </label>
               </div>
               <div>
-                <input
-                  className="input"
-                  type="text"
-                  placeholder="Enter your answer"
-                  onChange={handleAnswer}
+                <TextField
+                  id="standard-basic"
+                  label="Enter your answer"
+                  variant="standard"
                   value={usedAnswer}
+                  onChange={handleAnswer}
+                  className="input"
                 />
               </div>
               <div className="button">
@@ -89,7 +101,9 @@ const App = () => {
               </div>
             </div>
           ) : result < 2 ? (
-            <div>Sry you don't know your partner well. {result}</div>
+            <>
+              <div className="message">❤️❤️{slicedMessage}❤️❤️</div>
+            </>
           ) : (
             <div>You know your partner very very well Nice {result}</div>
           )}
