735c661fa607a75e8d3a06ecc625209b688ef37a
sajakshrestha8 <shresthasajak123@gmail.com>
2025-03-15 22:01:11 +0545

    login and signUp route created

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const express = require("express");
@@ -7,15 +8,15 @@ const dbConnection = require("./DBconnection/connection");
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send("Test");
 });
 
-app.post("/addDetails", (req, res) => {
-  let { question, answer } = req.body;
+app.post("/addDetails", async (req, res) => {
+  let { question, answer } = await req.body;
   const sql = `INSERT INTO data (Question, Answer) VALUES ("${question}", "${answer}")`;
 
-  dbConnection.query(sql, (err, result) => {
+  dbConnection.query(sql, async (err, result) => {
     if (err) throw err;
     console.log(result);
     res.status(200).json({
@@ -23,6 +24,39 @@ app.post("/addDetails", (req, res) => {
       question: question,
       answer: answer,
     });
+  });
+});
+
+app.post("/signUp", async (req, res) => {
+  let { email, password, role } = await req.body;
+  const sql = `INSERT INTO user (Email, Password, Role) VALUES ("${email}", "${password}", "${role}")`;
+
+  dbConnection.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(200).json({
+      messege: "Account created Successfully",
+      email: email,
+      role: role,
+    });
+  });
+});
+
+app.post("/login", async (req, res) => {
+  let { email, password } = await req.body;
+  const sql = `SELECT * FROM user WHERE Email = "${email}" AND Password = "${password}"`;
+
+  dbConnection.query(sql, (err, result) => {
+    if (err) throw err;
+
+    console.log(result);
+    if (result.length === 1) {
+      res.status(200).json({
+        messege: "Login Successfull",
+        email: email,
+      });
+    } else {
+      res.status(401).send("Wrong credentials");
+    }
   });
 });
 
